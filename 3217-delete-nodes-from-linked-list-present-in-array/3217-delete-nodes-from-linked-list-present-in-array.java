532483fe2024d04beb312bf9681f036f23448d10
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2025-07-02 09:38:34 +0530

    Time: 20 ms (88.25%), Space: 64.6 MB (56.77%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
@@ -2,27 +2,24 @@ import java.util.HashSet;
 
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        // Step 1: Add all nums into a HashSet for O(1) lookup
-        HashSet<Integer> deleteSet = new HashSet<>();
-        for (int num : nums) {
-            deleteSet.add(num);
+        HashSet<Integer>Hashset=new HashSet<>();
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode prev=dummy;
+        for(int num:nums){
+            Hashset.add(num);
         }
-
-        // Step 2: Use a dummy node to handle head deletion
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        ListNode curr = head;
-
-        while (curr != null) {
-            if (deleteSet.contains(curr.val)) {
-                prev.next = curr.next;  // delete current node
-            } else {
-                prev = curr;  // move prev only if current is not deleted
+        ListNode curr=head;
+        while(curr!=null){
+            if(Hashset.contains(curr.val)){
+                prev.next=curr.next;
             }
-            curr = curr.next;  // always move curr
+            else{
+                prev=curr;
+            }
+            curr=curr.next;
         }
-
         return dummy.next;
+
     }
 }
