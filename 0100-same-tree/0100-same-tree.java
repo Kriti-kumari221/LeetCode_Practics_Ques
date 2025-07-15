ed72f5681ac3e80c1913412dae1fa8e0047ba519
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2025-07-15 19:19:28 +0530

    Time: 0 ms (100%), Space: 41.2 MB (53.39%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -21,10 +21,9 @@ class Solution {
         if(p==null||q==null){
             return false;
         }
-        if(p.val !=q.val){
+        if(p.val!=q.val){
             return false;
         }
-       return isSameTree(p.left,q.left) &&
-        isSameTree(p.right,q.right);
+       return isSameTree(p.left,q.left)&& isSameTree(p.right,q.right);
     }
 }
