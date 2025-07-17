e76ccffb373e6e4fbf2d0f51727d4162c121536c
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2025-07-17 16:06:46 +0530

    Time: 0 ms (100%), Space: 41.6 MB (91.76%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -8,10 +8,10 @@ class Solution {
         int dp[][]=new int[n][m];
         dp[0][0]=1;
         for(int i=1;i<n;i++){
-            dp[i][0]=(obstacl[i][0]==0&&dp[0][0]==1)?1:0;
+            dp[i][0]=(obstacl[i][0]==0&&dp[i-1][0]==1)?1:0;
         }
         for(int j=1;j<m;j++){
-            dp[0][j]=(obstacl[0][j]==0&&dp[0][0]==1)?1:0;
+            dp[0][j]=(obstacl[0][j]==0&&dp[0][j-1]==1)?1:0;
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
