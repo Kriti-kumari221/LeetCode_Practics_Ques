1722ef88f71f3c43d7b392d14000f3ea5b962907
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2025-09-24 09:46:59 +0530

    Time: 1 ms (99.8%), Space: 41.2 MB (47.55%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -2,7 +2,7 @@ class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
         if(numerator==0)return "0";
         StringBuilder sb=new StringBuilder();
-        if((numerator<0)^(denominator<0))sb.append("_");
+        if((numerator<0)^(denominator<0))sb.append("-");
         long num=Math.abs((long) numerator);
         long den=Math.abs((long) denominator);
         sb.append(num/den);
