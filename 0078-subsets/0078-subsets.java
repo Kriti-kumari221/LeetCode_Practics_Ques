d02df504ef9751cf56fb53fd9f9cb58acaaf667f
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-01-24 16:21:46 +0530

    Time: 2 ms (6.38%), Space: 44.5 MB (14.24%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,16 +1,17 @@
 class Solution {
-    void rec(int []nums,int index,List<List<Integer>>res, List<Integer>output){
+    void recursion(int nums[],int index ,List<Integer>output,List<List<Integer>>res){
         res.add(new ArrayList<>(output));
         for(int i=index;i<nums.length;i++){
             output.add(nums[i]);
-            rec(nums,i+1,res,output);
+            recursion(nums,i+1,output,res);
             output.remove(output.size()-1);
         }
     }
     public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>>res=new ArrayList<>();
         List<Integer>output=new ArrayList<>();
-        rec(nums,0,res,output);
+        recursion(nums,0,output,res);
         return res;
     }
 }
