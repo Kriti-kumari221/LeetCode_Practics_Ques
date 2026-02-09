80e5f0a5561502983b061666f3231ae4a70d5f7d
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-02-09 10:27:16 +0530

    Time: 1 ms (100%), Space: 45.8 MB (0%) - LeetHub

--- 3833-count-dominant-indices/3833-count-dominant-indices.java
+++ 3833-count-dominant-indices/3833-count-dominant-indices.java
@@ -1,27 +1,20 @@
 class Solution {
     public int dominantIndices(int[] nums) {
-         int n = nums.length;
-        if (n <= 1) return 0;
-        int[] suffixSum = new int[n];
-        suffixSum[n - 1] = nums[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-            suffixSum[i] = suffixSum[i + 1] + nums[i];
-        }
-
-        int count = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            int sumRight = suffixSum[i + 1];
-            int elementsRight = n - i - 1;
-            double avgRight = (double) sumRight / elementsRight;
-
-            if (nums[i] > avgRight) {
+         int n=nums.length;
+         int totSum=0;
+         for(int x:nums)totSum+=x;
+         int lefts=0;
+         int right=0;
+         int count=0;
+         for(int i=0;i<n-1;i++){
+            lefts+=nums[i];
+            right=totSum-lefts;
+            int indCount=n-i-1;
+            double t=right/indCount;
+            if(nums[i]>t){
                 count++;
-         
             }
-        }
-
-        return count;
+         }
+         return count;
     }
 }
