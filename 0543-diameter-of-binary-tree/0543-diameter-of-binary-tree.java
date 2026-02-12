0a0c0650658be123ae2ad26dbebcc2deee77db72
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-02-12 11:41:22 +0530

    Time: 1 ms (45.99%), Space: 46.8 MB (91.28%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,19 +14,16 @@
  * }
  */
 class Solution {
-    int count=0;
+   int count=0;
     int fun(TreeNode root){
-        if(root==null){
-            return 0;
-        }
+        if(root==null)return 0;
         int left=fun(root.left);
         int right=fun(root.right);
         count=Math.max(count,left+right);
         return 1+Math.max(left,right);
     }
     public int diameterOfBinaryTree(TreeNode root) {
-fun(root);
-return count;
-
+        fun(root);
+        return count;
     }
 }
