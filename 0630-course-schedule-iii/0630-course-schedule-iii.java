8e6295827931e193a3e4c555db33a3ec243e2057
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-03-07 08:17:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.java
+++ 0630-course-schedule-iii/0630-course-schedule-iii.java
@@ -2,7 +2,6 @@ class Solution {
     public int scheduleCourse(int[][] courses) {
         Arrays.sort(courses,(a,b)->(a[1]-b[1]));
         PriorityQueue<Integer>q=new PriorityQueue<>((a,b)->b-a);
-
         int time=0;
         for(int c[]:courses){
             int d=c[0];
