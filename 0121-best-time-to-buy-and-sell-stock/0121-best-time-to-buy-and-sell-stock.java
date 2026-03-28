7739dee9d8572b082e426dc67d2609c9dd8f70d2
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-03-28 11:34:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,19 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-    int minprice=Integer.MAX_VALUE;
-    int max=0;
-    for (int i = 0; i < prices.length; i++) {
-        if (prices[i]<minprice) {
-            minprice=prices[i];
-        }
-        else{
-            max=Math.max(max,prices[i]-minprice);
-        }
-    }
-    return max;
-}
+    public int maxProfit(int[] nums) {
+        int n=nums.length;
+        int max=0;
+        int sum=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(nums[i]<sum){
+                sum=nums[i];
+            }
+            else{
+                max=Math.max(max,nums[i]-sum);
+            }
+            
+           
 
+        }
+         return max;
+    }
 }
