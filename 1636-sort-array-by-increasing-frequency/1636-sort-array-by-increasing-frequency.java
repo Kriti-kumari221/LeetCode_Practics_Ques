92f847dce9e3a2313ac0fdbeb0e62b9734ab4773
kriti <151952844+Kriti-kumari221@users.noreply.github.com>
2026-07-29 09:08:46 +0530

    Time: 3 ms (99.26%), Space: 45.2 MB (97.2%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,27 +1,18 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-
         int min = nums[0], max = nums[0];
-
         for (int num : nums) {
             min = Math.min(min, num);
             max = Math.max(max, num);
         }
-
         int[] count = new int[max - min + 1];
-
         for (int num : nums) {
             count[num - min]++;
         }
-
         int idx = 0;
-
         for (int freq = 1; freq <= nums.length; freq++) {
-
             for (int value = max; value >= min; value--) {
-
                 if (count[value - min] == freq) {
-
                     for (int k = 0; k < freq; k++) {
                         nums[idx++] = value;
                     }
